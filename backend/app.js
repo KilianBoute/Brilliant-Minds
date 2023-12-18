e2860d6487029fb919e0cd4f18212af692dff2cd
Kilian Boute <kilian.boute@gmail.com>
2023-12-18 14:57:06 +0100

    Add base route and search by id

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import connectDB from "./mariadb/connection.js";
+import mariadb from "mariadb";
 
 dotenv.config();
 
@@ -9,17 +9,49 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send({ message: "Hello world" });
-  res.end();
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || "Kilian",
+  password: process.env.DB_PASSWORD || "kilian",
+  database: process.env.DB_NAME || "brilliant-minds",
+  connectionLimit: 5,
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const connection = await pool.getConnection();
+    const data = await connection.query(`SELECT * FROM ideas`);
+    res.send(data);
+    connection.release();
+  } catch (err) {
+    console.error("Error connecting to MariaDB:", err);
+  } finally {
+    if (connection) connection.end();
+  }
+});
+
+app.get("/ideas/:id", async (req, res) => {
+  try {
+    const connection = await pool.getConnection();
+    const prepare = await connection.prepare(
+      "SELECT * FROM ideas WHERE id = ?"
+    );
+
+    const data = await prepare.execute([req.params.id]);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    if (connection) connection.end();
+  }
 });
 
 const startServer = async () => {
   try {
     app.listen(PORT, () => {
-      connectDB;
       console.log(`App started on port ${PORT}`);
-      console.log(new Date);
+      console.log(new Date());
     });
   } catch (err) {
     console.log(err);
